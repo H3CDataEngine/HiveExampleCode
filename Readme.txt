588ea0dbf5e633025c68972134434889978cbaac
H3CDataEngine <48853458+H3CDataEngine@users.noreply.github.com>
2020-02-12 21:35:55 +0800

    提交代码

---
+++ Readme.txt
@@ -0,0 +1,13 @@
+﻿该工程为H3C DataEngine产品中Hive组件提供样例代码，而且区分kerberos环境和非kerberos环境。该工程的目录结构介绍：
+
+HiveExampleCode/
+  |Readme.txt                      --介绍文档
+  |hive-example-normal/       --非Kerberos环境下样例代码
+     |pom.xml                      --pom文件
+     |src/	                   --hive增删查改样例代码
+  |hive-example-security/     --Kerberos环境下样例代码
+     |pom.xml			   --pom文件
+     |src/	                   --hive增删查改样例代码
+     
+修改示例中url等变量，可以直接在IDEA上执行，或者编译打包生成带依赖的Jar包，在客户端服务器上执行如下命令：
+java -jar hivedemo-1.0-SNAPSHOT-jar-with-dependencies.jar com.h3c.hive.SimpleExample
---
+++ hive-example-security/src/main/java/com/h3c/hive/SimpleExample.java
@@ -0,0 +1,143 @@
+package com.h3c.hive;
+
+import org.apache.hadoop.security.UserGroupInformation;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SimpleExample {
+    private static String driveName = "org.apache.hive.jdbc.HiveDriver" ;
+    private static String url = "jdbc:hive2://101.8.202.1:10010/default;principal=hive/node1.hde.h3c.com@HDE.H3C.COM";
+    private static String user = "hive";
+    private static String passwd = null;
+    private static String sql = "";
+    private static ResultSet res;
+    private static ResultSetMetaData m;
+
+    public static void main(String[] args){
+        Connection con = null;
+        Statement stm = null;
+
+        try{
+            con = getConnection();
+            stm = con.createStatement();
+
+            String tableName = "stu2";
+            dropTable(stm,tableName);
+            createTable(stm,tableName);
+            showTables(stm, tableName);
+            describeTale(stm, tableName);
+            selectData(stm, tableName);
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+            System.out.println(driveName + " not found! ");
+            System.out.println(e.getMessage());
+        }catch (SQLException e1){
+            e1.printStackTrace();
+            System.out.println("connection error! ");
+            System.out.println(e1.getMessage());
+        }finally {
+            try{
+                if(res!=null){
+                    res.close();
+                    res=null;
+                }
+                if(stm!=null){
+                    stm.close();
+                    stm=null;
+                }
+                if(con!=null){
+                    con.close();
+                    con=null;
+                }
+            }catch (SQLException e2){
+                e2.printStackTrace();
+                System.out.println("close connection or statement error! " );
+                System.out.println(e2.getMessage());
+            }
+        }
+    }
+
+    private static Connection getConnection() throws ClassNotFoundException,SQLException{
+        String krb5ConfPath = "./krb5.conf";
+        String keytabPath = "./hive.service.keytab";
+        String principalName = "hive/node1.hde.h3c.com@HDE.H3C.COM";
+        org.apache.hadoop.conf.Configuration conf = new org.apache.hadoop.conf.Configuration();
+        conf.set("hadoop.security.authentication", "Kerberos");
+
+        //获取krb5.conf配置
+        if(System.getProperty("os.name").toLowerCase().startsWith("win")) {
+            System.setProperty("java.security.krb5.conf", SimpleExample.class.getClassLoader().getResource(krb5ConfPath).getPath());
+        }
+
+        //进行身份认证
+        try{
+            UserGroupInformation.setConfiguration(conf);
+            UserGroupInformation.loginUserFromKeytab(principalName,SimpleExample.class.getClassLoader().getResource(keytabPath).getPath());
+        }catch(IOException e1){
+            e1.printStackTrace();
+        }
+
+        Class.forName(driveName);
+        Connection con = DriverManager.getConnection(url,user,passwd);
+        System.out.println("connection success!");
+        return con;
+    }
+
+    private static void dropTable(Statement stm, String tableName)throws SQLException{
+        sql = "drop table if exists "+tableName;
+        System.out.println("Running:"+sql);
+        stm.executeUpdate(sql);
+    }
+
+    private static void createTable(Statement stm, String tableName)throws SQLException{
+        sql = "create table if not exists "+tableName+" (stuid string, name string, sex string, age int)"
+                +" row format delimited fields terminated by '\t'"
+                + " lines terminated by '\n' "
+                + " stored as textfile";
+        System.out.println("Running:"+sql);
+        stm.executeUpdate(sql);
+    }
+
+    private static void showTables(Statement stm, String tableName)throws SQLException{
+        sql = "show tables in default";
+        System.out.println("Running:"+sql);
+        res = stm.executeQuery(sql);
+        System.out.println("执行 show tables的运行结果如下：");
+        m=res.getMetaData();
+        int columns=m.getColumnCount();
+        while(res.next())
+        {
+            for(int i=1;i<=columns;i++)
+            {
+                System.out.print(res.getString(i)+"\t\t");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void describeTale(Statement stm, String tableName)throws SQLException{
+        sql = "describe " + tableName;
+        System.out.println("Running:"+sql);
+        res = stm.executeQuery(sql);
+        System.out.println("执行 describe tables的运行结果如下：");
+        while(res.next()){
+            System.out.println(res.getString(1) + "\t" + res.getString(2));
+        }
+    }
+
+    private static void selectData(Statement stm, String tableName)throws SQLException{
+        sql = "select * from "+tableName;
+        System.out.println("Running:"+sql);
+        res = stm.executeQuery(sql);
+        System.out.println("执行 select * 的运行结果如下：");
+        while(res.next()){
+            System.out.println(res.getString(1) + "\t" + res.getString(2) + "\t"
+                    + res.getString(3) + "\t" + res.getString(4));
+        }
+    }
+}
